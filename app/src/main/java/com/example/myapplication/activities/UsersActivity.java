ba7bab5f948beb79b7e4d741f0e43b68fef6ee04
TengNg <nguyentin@gmail.com>
2023-07-27 22:46:03 +0700

    fix: UserList showing only nonfriend Users

--- app/src/main/java/com/example/myapplication/activities/UsersActivity.java
+++ app/src/main/java/com/example/myapplication/activities/UsersActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,17 +15,24 @@ import com.example.myapplication.listeners.UserListener;
 import com.example.myapplication.models.User;
 import com.example.myapplication.utilities.Constant;
 import com.example.myapplication.utilities.PreferenceManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -32,7 +40,6 @@ public class UsersActivity extends AppCompatActivity implements UserListener {
     private ActivityUsersBinding binding;
     private PreferenceManager preferenceManager;
     private List<User> users;
-    private Set<String> friendIds;
     private UsersAdapter usersAdapter;
     private FirebaseFirestore db;
 
@@ -45,12 +52,11 @@ public class UsersActivity extends AppCompatActivity implements UserListener {
         this.binding.progressCircular.setVisibility(View.VISIBLE);
         this.handleBackPressed();
         initialize();
-        listenUsers();
+        getUsers();
     }
 
     private void initialize() {
         this.users = new ArrayList<>();
-        this.friendIds = new HashSet<>();
         this.usersAdapter = new UsersAdapter(this.users, this);
         this.db = FirebaseFirestore.getInstance();
         this.binding.usersRecyclerView.setAdapter(usersAdapter);
@@ -64,65 +70,90 @@ public class UsersActivity extends AppCompatActivity implements UserListener {
         this.binding.backButton.setOnClickListener(v -> onBackPressed());
     }
 
-    private void listenUsers() {
-        db.collection(Constant.KEY_COLLECTION_USERS)
-                .addSnapshotListener(eventListener);
-    }
+    public void getUsers() {
+        CollectionReference usersCollection = this.db.collection(Constant.KEY_COLLECTION_USERS);
+        String specificUserID = this.preferenceManager.getString(Constant.KEY_USER_ID);
+        CollectionReference specificUserFriendsRef = usersCollection.document(specificUserID).collection(Constant.KEY_COLLECTION_USER_FRIENDS);
 
-    private final EventListener<QuerySnapshot> eventListener = (value, error) -> {
-        if (error != null) {
-            return;
-        }
+        specificUserFriendsRef.get().addOnSuccessListener(friendsQuerySnapshot -> {
+            List<String> friendIDs = new ArrayList<>();
+            for (DocumentSnapshot documentSnapshot : friendsQuerySnapshot.getDocuments()) {
+                String friendID = documentSnapshot.getData().keySet().iterator().next(); // get only first field in a document
+                friendIDs.add(friendID);
+            }
 
-        if (value != null) {
-            for (DocumentChange documentChange : value.getDocumentChanges()) {
-                if (documentChange.getDocument().getId().equals(this.preferenceManager.getString(Constant.KEY_USER_ID)))
-                    continue;
+            usersCollection.get().addOnSuccessListener(usersQuerySnapshot -> {
+                List<DocumentSnapshot> allUsers = usersQuerySnapshot.getDocuments();
+                List<DocumentSnapshot> nonFriendUsers = new ArrayList<>();
 
-                if (documentChange.getType() == DocumentChange.Type.ADDED) {
+                for (DocumentSnapshot user : allUsers) {
+                    String userID = user.getId();
+                    if (!friendIDs.contains(userID) && !userID.equals(specificUserID)) {
+                        nonFriendUsers.add(user);
+                    }
+                }
+
+                for (DocumentSnapshot document : nonFriendUsers) {
                     User u = new User();
-                    u.email = documentChange.getDocument().getString(Constant.KEY_EMAIL);
-                    u.name = documentChange.getDocument().getString(Constant.KEY_NAME);
-                    u.token = documentChange.getDocument().getString(Constant.KEY_FCM_TOKEN);
-                    u.id = documentChange.getDocument().getId();
+                    u.email = document.getString(Constant.KEY_EMAIL);
+                    u.name = document.getString(Constant.KEY_NAME);
+                    u.token = document.getString(Constant.KEY_FCM_TOKEN);
+                    u.id = document.getId();
                     this.users.add(u);
                     this.usersAdapter.notifyDataSetChanged();
                 }
-//                else if (documentChange.getType() == DocumentChange.Type.MODIFIED) {
-//                    String changedModelEmail = documentChange.getDocument().getString(Constant.KEY_EMAIL);
-//                    for (int i = 0; i < this.users.size(); i++) {
-//                        if (this.users.get(i).email.equals(changedModelEmail)) {
-//                            this.users.get(i).token = documentChange.getDocument().getString(Constant.KEY_FCM_TOKEN);
-//                            this.usersAdapter.notifyItemChanged(i);
-//                        }
-//                    }
-//                }
-            }
-        } else {
+
+                this.binding.progressCircular.setVisibility(View.GONE);
+            }).addOnFailureListener(e -> {
+                this.showErrorMsg();
+            });
+        }).addOnFailureListener(e -> {
             this.showErrorMsg();
-        }
-
-        this.binding.progressCircular.setVisibility(View.GONE);
-    };
-
-    private void getFriendIds() {
-        String specificUserID = this.preferenceManager.getString(Constant.KEY_USER_ID);
-        CollectionReference specificUserFriendsRef = db.collection(Constant.KEY_COLLECTION_USERS).document(specificUserID).collection(Constant.KEY_COLLECTION_USER_FRIENDS);
-
-        specificUserFriendsRef.get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    Map<String, Object> map = document.getData();
-
-                    if (map == null) continue;
-
-                    // get all friend ids
-                    for (Map.Entry<String, Object> entry : map.entrySet()) {
-                        this.friendIds.add(entry.getKey());
-                    }
-                }
-            }
         });
+
+//        Task<QuerySnapshot> usersTask = usersCollection.get();
+//        usersTask.continueWithTask(task -> {
+//            CollectionReference specificUserFriendsRef = usersCollection.document(specificUserID).collection(Constant.KEY_COLLECTION_USER_FRIENDS);
+//            return specificUserFriendsRef.get();
+//        }).addOnSuccessListener(friendsQuerySnapshot -> {
+//            List<DocumentSnapshot> allUsers = usersTask.getResult().getDocuments();
+//            List<DocumentSnapshot> nonFriendUsers = new ArrayList<>();
+//            List<String> friendIDs = new ArrayList<>();
+//
+//            for (DocumentSnapshot friend : friendsQuerySnapshot.getDocuments()) {
+//                friendIDs.add(friend.getId());
+//                String id = friend.getData().keySet().iterator().next(); // get only first field in a document
+//                friendIDs.add(id);
+//            }
+//
+//            for (DocumentSnapshot user : allUsers) {
+//                String userID = user.getId();
+//
+//                if (userID.equals(specificUserID))
+//                    continue;
+//
+//                if (friendIDs.contains(userID))
+//                    continue;
+//
+//                nonFriendUsers.add(user);
+//            }
+//
+//            for (DocumentSnapshot document : nonFriendUsers) {
+//                User u = new User();
+//                u.email = document.getString(Constant.KEY_EMAIL);
+//                u.name = document.getString(Constant.KEY_NAME);
+//                u.token = document.getString(Constant.KEY_FCM_TOKEN);
+//                u.id = document.getId();
+//                this.users.add(u);
+//                this.usersAdapter.notifyDataSetChanged();
+//            }
+//
+//            this.binding.progressCircular.setVisibility(View.GONE);
+//
+//        }).addOnFailureListener(e -> {
+//            this.showErrorMsg();
+//        });
+
     }
 
     @Override
@@ -151,6 +182,26 @@ public class UsersActivity extends AppCompatActivity implements UserListener {
 //        Toast.makeText(getApplicationContext(), "Friend added", Toast.LENGTH_SHORT).show();
 
         // TODO: send notification to NotificationActivity & FriendsActivity
+        String senderId = this.preferenceManager.getString(Constant.KEY_USER_ID);
+        String receiverId = user.id;
+        String friendRequestID = this.db.collection(Constant.KEY_COLLECTION_FRIEND_REQUESTS).document().getId();
+        HashMap<String, Object> friendRequestData = new HashMap<>();
+        friendRequestData.put("senderID", senderId);
+        friendRequestData.put("receiverID", receiverId);
+        friendRequestData.put("status", "pending");
+        friendRequestData.put("timestamp", new Date());
+
+        this.db.collection(Constant.KEY_COLLECTION_FRIEND_REQUESTS)
+                .document(friendRequestID)
+                .set(friendRequestData)
+                .addOnSuccessListener(aVoid -> {
+                    // Friend request sent successfully
+                    Toast.makeText(getApplicationContext(), "Friend request sent", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    // Error occurred while sending friend request
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                });
     }
 
     @Override
@@ -158,5 +209,9 @@ public class UsersActivity extends AppCompatActivity implements UserListener {
         Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
         intent.putExtra(Constant.KEY_USER, user);
         startActivity(intent);
+    }
+
+    private String getSimpleMessageDateTime(Date date) {
+        return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
     }
 }
