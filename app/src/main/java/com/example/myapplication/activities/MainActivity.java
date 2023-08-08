3271b57866fe0f51e45d545ca049c3e20beef1a5
TengNg <nguyentin@gmail.com>
2023-08-08 23:53:19 +0700

    feat: adding navigation

--- app/src/main/java/com/example/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.myapplication.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
-import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.R;
 import com.example.myapplication.adapters.RecentConversationsAdapter;
 import com.example.myapplication.databinding.ActivityMainBinding;
 import com.example.myapplication.listeners.ConversationsListener;
@@ -18,10 +27,12 @@ import com.example.myapplication.models.ChatMessage;
 import com.example.myapplication.models.User;
 import com.example.myapplication.utilities.Constant;
 import com.example.myapplication.utilities.PreferenceManager;
-import com.google.firebase.firestore.CollectionReference;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -32,6 +43,7 @@ import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -41,17 +53,34 @@ public class MainActivity extends AppCompatActivity implements ConversationsList
     PreferenceManager preferenceManager;
     RecentConversationsAdapter recentConversationsAdapter;
     FirebaseFirestore db;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(this.binding.getRoot());
+
         this.getToken();
         this.initialize();
         this.loadUserImage();
+
+        this.setupNavDrawerView();
+        this.setupNavBottomView();
+
         this.listenConversations();
         this.handleOpenMenu();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void initialize() {
@@ -74,7 +103,18 @@ public class MainActivity extends AppCompatActivity implements ConversationsList
         byte[] bytes = Base64.decode(preferenceManager.getString(Constant.KEY_IMAGE), Base64.DEFAULT);
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
         this.binding.profileImage.setImageBitmap(bitmap);
-        this.binding.profileImage.setImageBitmap(bitmap);
+
+        String username = preferenceManager.getString(Constant.KEY_NAME);
+        String email = preferenceManager.getString(Constant.KEY_EMAIL);
+
+        View headerView = binding.navView.getHeaderView(0);
+        TextView usernameTextView = headerView.findViewById(R.id.nav_header_name);
+        TextView emailTextView = headerView.findViewById(R.id.nav_header_email);
+        ImageView userImage = headerView.findViewById(R.id.nav_header_image);
+
+        usernameTextView.setText(username);
+        emailTextView.setText(email);
+        userImage.setImageBitmap(bitmap);
     }
 
     private void getToken() {
@@ -174,6 +214,94 @@ public class MainActivity extends AppCompatActivity implements ConversationsList
         }
 
         return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date1);
+    }
+
+    private void setupNavBottomView() {
+        bottomNavigationView = findViewById(R.id.nav_bottom);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            } else if (id == R.id.nav_contacts) {
+                if (!isUsersActivityVisible()) {
+                    startActivity(new Intent(this, FriendsActivity.class));
+                    return true;
+                }
+            } else if (id == R.id.nav_notifications) {
+                startActivity(new Intent(this, NotificationActivity.class));
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void setupNavDrawerView() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            // Handle item clicks in the Navigation Drawer here
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                startActivity(new Intent(this, MainActivity.class));
+            } else if (id == R.id.nav_contacts) {
+                drawerLayout.closeDrawers();
+                startActivity(new Intent(this, FriendsActivity.class));
+            } else if (id == R.id.nav_find_friends) {
+                drawerLayout.closeDrawers();
+                if (!isUsersActivityVisible()) {
+                    startActivity(new Intent(this, UsersActivity.class));
+                }
+            } else if (id == R.id.nav_settings) {
+                showToast("Settings clicked");
+            } else if (id == R.id.nav_logout) {
+                drawerLayout.closeDrawers();
+                signOut();
+                return true;
+            }
+            drawerLayout.closeDrawers();
+            return true;
+        });
+    }
+
+    private boolean isUsersActivityVisible() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> runningTasks = activityManager.getRunningTasks(Integer.MAX_VALUE);
+
+        for (ActivityManager.RunningTaskInfo runningTaskInfo : runningTasks) {
+            ComponentName topActivity = runningTaskInfo.topActivity;
+            if (topActivity.getClassName().equals(UsersActivity.class.getName())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void signOut() {
+        showToast("Signing out...");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference dr = db.collection(Constant.KEY_COLLECTION_USERS).document(this.preferenceManager.getString(Constant.KEY_USER_ID));
+
+        HashMap<String, Object> newData = new HashMap<>();
+        newData.put(Constant.KEY_FCM_TOKEN, FieldValue.delete());
+        newData.put(Constant.KEY_IS_AVAILABLE, false);
+        dr.update(newData)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(ex -> {
+                    showToast("Unable to sign out");
+                });
     }
 
     @Override
