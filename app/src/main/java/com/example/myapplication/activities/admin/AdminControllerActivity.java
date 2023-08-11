847f9cbccce49da6cc2b5b440e13eed4d417122e
TengNg <nguyentin@gmail.com>
2023-08-11 15:57:38 +0700

    wip: Admin role

---
+++ app/src/main/java/com/example/myapplication/activities/admin/AdminControllerActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.activities.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class AdminControllerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_controller);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/activities/admin/ShowUserFriendListActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.activities.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class ShowUserFriendListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_user_friend_list);
+    }
+}
